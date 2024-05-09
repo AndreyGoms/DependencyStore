52380da9ebc688f22cd915802783f2cb31cfff54
Andrey Gomes <andreygm74@gmail.com>
2024-05-09 01:00:15 -0300

    Initial commit

---
+++ Services/DeliveryFeeService.cs
@@ -0,0 +1,21 @@
+using DependencyStore.Services.Contracts;
+using RestSharp;
+
+namespace DependencyStore.Services;
+
+public class DeliveryFeeService : IDeliveryFeeService
+{
+    public async Task<decimal> GetDeliveryFeeAsync(string zipCode)
+    {
+        //https://consultafrete.io/cep/
+        var client = new RestClient("http://api.myorg.com");
+        var request = new RestRequest()
+            .AddJsonBody( new 
+            {
+                ZipCode = zipCode   
+            });
+
+        var response = await client.PostAsync<decimal>(request);
+        return response < 5 ? 5 : response;
+    }
+}
