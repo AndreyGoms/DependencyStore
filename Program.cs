63b147091458d880b51bd3e202869279a48f856b
Andrey Gomes <andreygm74@gmail.com>
2024-05-10 12:40:51 -0300

    Resolvendo dependencias na Program e criando class de configuração.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
+using DependencyStore;
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<Configuration>();
+builder.Services.AddScoped(x 
+    => new SqlConnection("CONN_STRING"));
+
+//Caso não haja camada de interface, pode ser usado dessa maneira:
+//builder.Services.AddTransient<CustomerRepository>();
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
--- Services/DeliveryFeeService.cs
+++ Services/DeliveryFeeService.cs
@@ -5,10 +5,15 @@ namespace DependencyStore.Services;
 
 public class DeliveryFeeService : IDeliveryFeeService
 {
+    private readonly Configuration _configuration;
+
+    public DeliveryFeeService(Configuration configuration) => _configuration = configuration;
+
     public async Task<decimal> GetDeliveryFeeAsync(string zipCode)
     {
         //https://consultafrete.io/cep/
-        var client = new RestClient("http://api.myorg.com");
+        //var client = new RestClient("http://api.myorg.com");
+        var client = new RestClient(_configuration.DeliveryFeeServiceUrl);
         var request = new RestRequest()
             .AddJsonBody( new 
             {
